e6bc48e1d2546d8c529bce7aca5140195b3dd931
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2021-03-18 15:10:48 +0100

    przeszło testy

--- accum.js
+++ accum.js
@@ -5,19 +5,27 @@ accum("abcd") -> "A-Bb-Ccc-Dddd"
 accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.*/
-function accum(tekst){
-    if (tekst.includes(1,2,3,4,5,6,7,8,9,0,null)) console.log("wpisz ciąg zawierający tylko litery");
-    const wynik=[];
-const a=tekst.length;
-tekst=[];
-let i=0;
-let j=1;
-do {
+function accum(tekst) {
+  if (tekst.includes(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, null))
+    console.log("wpisz ciąg zawierający tylko litery");
 
-i++
-}
-while (i<=a);
-console.log(wynik);
+  tekst = tekst.toLowerCase();
+  const res = [];
+  let i = 0;
+  //console.log(res);
+  while (i <= tekst.length - 1) {
+    let wypelniana = new Array(i + 1);
+
+    wypelniana.fill(tekst[i]);
+    wypelniana[0] = wypelniana[0].toUpperCase();
+    wypelniana = wypelniana.join("");
+    res.push(wypelniana);
+    i++;
+    // console.log(res);
+  }
+  const result = res.join("-");
+  console.log(result);
+  return result;
 }
 
-accum("Maciek");
+accum("cwAt");
